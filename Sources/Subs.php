d08952e4bc4ab0234511401fea2bc0767730a6d6
Steven McDonald <steven@steven-mcdonald.id.au>
2014-03-14 16:41:55 +1100

    Add an ipaddr column to log_online, using PostgreSQL's native inet type

--- Sources/Subs.php
+++ Sources/Subs.php
@@ -2683,7 +2683,7 @@ function writeLog($force = false)
 
 		$smcFunc['db_query']('', '
 			UPDATE {db_prefix}log_online
-			SET log_time = {int:log_time}, ip = IFNULL(INET_ATON({string:ip}), 0), url = {string:url}
+			SET log_time = {int:log_time}, ip = IFNULL(INET_ATON({string:ip}), 0), ipaddr = \'' . $user_info['ip'] . '\', url = {string:url}
 			WHERE session = {string:session}',
 			array(
 				'log_time' => time(),
@@ -2715,8 +2715,8 @@ function writeLog($force = false)
 
 		$smcFunc['db_insert']($do_delete ? 'ignore' : 'replace',
 			'{db_prefix}log_online',
-			array('session' => 'string', 'id_member' => 'int', 'id_spider' => 'int', 'log_time' => 'int', 'ip' => 'raw', 'url' => 'string'),
-			array($session_id, $user_info['id'], empty($_SESSION['id_robot']) ? 0 : $_SESSION['id_robot'], time(), 'IFNULL(INET_ATON(\'' . $user_info['ip'] . '\'), 0)', $serialized),
+			array('session' => 'string', 'id_member' => 'int', 'id_spider' => 'int', 'log_time' => 'int', 'ip' => 'raw', 'ipaddr' => 'raw', 'url' => 'string'),
+			array($session_id, $user_info['id'], empty($_SESSION['id_robot']) ? 0 : $_SESSION['id_robot'], time(), 'IFNULL(INET_ATON(\'' . $user_info['ip'] . '\'), 0)', '\'' . $user_info['ip'] . '\'', $serialized),
 			array('session')
 		);
 	}

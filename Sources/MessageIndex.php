8a8848e38dab831238fea38bcdf4c0565937a76d
SexWarrior <svarrior@tfes.org>
2019-04-22 21:17:15 +0100

    Once again, let's not break everything.

--- Sources/MessageIndex.php
+++ Sources/MessageIndex.php
@@ -55,7 +55,7 @@ function MessageIndex()
 
 	// View all the topics, or just a few?
 	$context['topics_per_page'] = empty($modSettings['disableCustomPerPage']) && !empty($options['topics_per_page']) ? $options['topics_per_page'] : $modSettings['defaultMaxTopics'];
-	$context['messages_per_page'] = empty($modSettings['disableCustomPerPage']) && !empty($options['messages_per_page']) $options['messages_per_page'] : $modSettings['defaultMaxMessages'];
+	$context['messages_per_page'] = empty($modSettings['disableCustomPerPage']) && !empty($options['messages_per_page']) ? $options['messages_per_page'] : $modSettings['defaultMaxMessages'];
 	$maxindex = isset($_REQUEST['all']) && !empty($modSettings['enableAllMessages']) ? $board_info['total_topics'] : $context['topics_per_page'];
 
 	// Right, let's only index normal stuff!
